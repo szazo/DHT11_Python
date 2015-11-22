ed6602ca5cf3fc6b4f76aaa25df276b0e5e03f64
szazo <szazo@neting.hu>
2015-11-22 21:20:32 +0100

    Added DHT11 class

---
+++ README.md
@@ -0,0 +1,1 @@
+# DHT11_Python
---
+++ dht11_example.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+pin = 14
+instance = dht11.DHT11(pin)
+
+while True:
+	result = instance.read()
+	if result.is_valid():
+		print("Last valid input: " + str(datetime.datetime.now()))
+		print("Temperature: %d C" % result.temperature)
+		print("Humidity: %d %%" % result.humidity)
+	
+	time.sleep(1)
+
+GPIO.cleanup()
